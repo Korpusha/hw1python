c3b078847842a4d5cfedad65044c9dcb37776b51
Korpusha <daniilzzz45@gmail.com>
2021-10-06 13:28:04 +0300

    Fixes

--- class_good.py
+++ class_good.py
@@ -2,7 +2,10 @@
 
 
 class Goods:
-    def __init__(self, price, name, category, unit_of_measurement):
+    allowed_categories = ('supply', 'household chemistry', 'alcohol')
+    not_allowed_units = ('kg', 'kilogrammes', 'kilo', 'kilogram', 'kilogramme')
+
+    def __init__(self, price, name, unit_of_measurement, category):
         self.price = price
         self.category = category
         self.name = name
@@ -14,8 +17,7 @@ class Goods:
         Allowed only categories to check: 'supply', 'household chemistry', 'alcohol'
         Also used to help method totally_eatable in class Basket
         """
-        allowed_categories = ('supply', 'household chemistry', 'alcohol')
-        if self.category in allowed_categories and self.category != 'household chemistry':
+        if self.category != 'household chemistry':
             return True
         return False
 
@@ -26,23 +28,22 @@ class Goods:
         If 'by number', then it multiplies the price with its number
         Also used to help method total in class Basket
         """
-        not_allowed_units = ('kg', 'kilogrammes', 'kilo', 'kilogram', 'kilogramme')
         for i in self.unit_of_measurement.split():
-            if i in not_allowed_units:
+            if i in Goods.not_allowed_units:
                 return self.price
-        if self.unit_of_measurement not in not_allowed_units:
-            our_num = int("".join(filter(str.isdigit, self.unit_of_measurement)))  # gets number out of string
-            return self.price * our_num
+        else:
+            return self.price * float(self.unit_of_measurement.split()[0])
 
 
 class Basket:
-    def __init__(self, what_in):
+    def __init__(self, what_in, amount):
         self.what_in = what_in
+        self.amount = amount
 
-    def total(self):
+    def my_total(self):
         """Same as price_total method, but sums them all together"""
-        total_sum = [Goods.price_total(i) for i in self.what_in]
-        return sum(total_sum)
+        total = [Goods.price_total(product) * number for product, number in zip(self.what_in, self.amount)]
+        return f'Сумма покупок: {sum(total)}'
 
     def totally_eatable(self):
         """Same as is_eatable method, but if at least one uneatable found - returns 'Uneatable'"""
@@ -52,13 +53,12 @@ class Basket:
         return 'Eatable'
 
 
-first_good = Goods(5, 'juice', '3 things', 'supply')
+first_good = Goods(5, 'juice', '30 things', 'supply')
 second_good = Goods(15, 'ham', '2 things', 'supply')
 third_good = Goods(50, 'whiskey', '1 thing', 'alcohol')
 fourth_good = Goods(25, 'shampoo', '2 things', 'household chemistry')
 fifth_good = Goods(30, 'buckwheat', '2 kilo', 'supply')
-my_basket = Basket((first_good, second_good, third_good, fourth_good, fifth_good))
+my_basket = Basket([first_good, second_good, third_good, fourth_good, fifth_good], [1, 2, 3, 4, 5])
 
-print(fourth_good.is_eatable())  # False
-print(my_basket.total())  # 175
-print(my_basket.totally_eatable())  # Uneatable
+print(my_basket.my_total())
+print(my_basket.totally_eatable())
