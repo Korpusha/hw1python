7f9add016ea5c55de5a08b3b4d5f23427ae2ab0e
Korpusha <daniilzzz45@gmail.com>
2021-10-06 19:48:32 +0300

    Changes in Basket

--- class_good.py
+++ class_good.py
@@ -2,7 +2,7 @@
 
 
 class Goods:
-    allowed_categories = ('supply', 'household chemistry', 'alcohol')
+    allowed_eatable_categories = ('supply', 'alcohol')
     not_allowed_units = ('kg', 'kilogrammes', 'kilo', 'kilogram', 'kilogramme')
 
     def __init__(self, price, name, unit_of_measurement, category):
@@ -17,7 +17,7 @@ class Goods:
         Allowed only categories to check: 'supply', 'household chemistry', 'alcohol'
         Also used to help method totally_eatable in class Basket
         """
-        if self.category != 'household chemistry':
+        if self.category not in Goods.allowed_eatable_categories:
             return True
         return False
 
@@ -31,18 +31,16 @@ class Goods:
         for i in self.unit_of_measurement.split():
             if i in Goods.not_allowed_units:
                 return self.price
-        else:
             return self.price * float(self.unit_of_measurement.split()[0])
 
 
 class Basket:
-    def __init__(self, what_in, amount):
+    def __init__(self, what_in):
         self.what_in = what_in
-        self.amount = amount
 
     def my_total(self):
         """Same as price_total method, but sums them all together"""
-        total = [Goods.price_total(product) * number for product, number in zip(self.what_in, self.amount)]
+        total = [Goods.price_total(product) for product in self.what_in]
         return f'Сумма покупок: {sum(total)}'
 
     def totally_eatable(self):
@@ -58,7 +56,7 @@ second_good = Goods(15, 'ham', '2 things', 'supply')
 third_good = Goods(50, 'whiskey', '1 thing', 'alcohol')
 fourth_good = Goods(25, 'shampoo', '2 things', 'household chemistry')
 fifth_good = Goods(30, 'buckwheat', '2 kilo', 'supply')
-my_basket = Basket([first_good, second_good, third_good, fourth_good, fifth_good], [1, 2, 3, 4, 5])
+my_basket = Basket([first_good, second_good, third_good, fourth_good, fifth_good])
 
 print(my_basket.my_total())
 print(my_basket.totally_eatable())
